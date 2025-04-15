081ee7ee237bb11d3e0a50092be2aa01862c7b50
Har1sh-k <hkolla03@gmail.com>
2025-04-15 14:43:06 -0500

    time logger for signal dataset extraction

---
+++ src/signal generator/signal_time_logger.py
@@ -0,0 +1,172 @@
+from datetime import datetime, timedelta
+import os
+import argparse
+import subprocess
+
+import logging
+
+TIME_DURATION = timedelta(minutes=30)
+
+logging.basicConfig(
+    filename='src\history_status.log',
+    filemode='a',
+    format='%(asctime)s, %(msecs)d %(name)s - %(levelname)s : %(message)s',
+    datefmt='%H:%M:%S',
+    level=logging.DEBUG  
+)
+
+level_map = {
+    'debug': logging.debug,
+    'info': logging.info,
+    'warning': logging.warning,
+    'error': logging.error,
+    'critical': logging.critical,
+}
+def logger(level_name,message):
+
+        log_func = level_map[level_name.lower()]
+        if log_func:
+            log_func(message)
+        else:
+            logging.error(f"Invalid logging level: {level_name}")
+            
+            
+## FLOODING ATTACKS            
+def bruteforce_char(ip_addr):
+    
+    command= f'sudo python3 bruteforce.py {ip_addr} sample_username'
+    
+    
+    return
+
+def dos_imcp(ip_addr):
+    
+    return
+
+def dos_tcp(ip_addr):
+    
+    return
+    
+    
+## IMPERSONATION ATTACKS
+def arp_spoof(ip_addr):
+        
+    return
+
+def arp_eveadropping(ip_addr):
+        
+    return
+
+def arp_blackout(ip_addr):
+        
+    return
+
+## Injection ATTACKS
+def tcp_flag_injection():
+    
+    return
+
+def replay_attack():
+    
+    return
+
+def fdia_attack():
+    
+    return
+            
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description='DS/ SIGNAL GENERATION')
+    parser.add_argument('target1', help='IP of first target')
+    parser.add_argument('target2', help='IP of second target')
+    
+    TIME_DURATION = timedelta(minutes=30)
+    
+    args = parser.parse_args()
+    
+    IP_ADDR_1= args.target1
+    IP_ADDR_2=args.target2
+    
+    # FLOODING
+    ## Bruteforce attack
+    tick=datetime.now()
+    logger('info',f"Starting Bruteforce attack on {IP_ADDR_1} at {tick}")
+    bruteforce_char(IP_ADDR_1)
+    tock=datetime.now()
+    logger('info',f"Finished Bruteforce attack on {IP_ADDR_1} at {tock}")
+    logger('info',f"Duration of Bruteforce attack on {IP_ADDR_1}: {tock-tick}")
+    
+    ## ICMP Flooding attack
+    tick=datetime.now()
+    logger('info',f"Starting ICMP flooding attack on {IP_ADDR_1} at {tick}")
+    dos_imcp(IP_ADDR_1)
+    tock=datetime.now()
+    logger('info',f"Finished ICMP flooding attack on {IP_ADDR_1} at {tock}")
+    logger('info',f"Duration of ICMP flooding attack on {IP_ADDR_1}: {tock-tick}")
+    
+    ## TCP Flooding attack
+    tick=datetime.now()
+    logger('info',f"Starting TCP flooding attack on {IP_ADDR_1} at {tick}")
+    dos_tcp(IP_ADDR_1)
+    tock=datetime.now()
+    logger('info',f"Finished TCP flooding attack on {IP_ADDR_1} at {tock}")
+    logger('info',f"Duration of TCP flooding attack on {IP_ADDR_1}: {tock-tick}")
+    
+    # IMPERSONATION ATTACKS
+    ## ARP Spoofing attack
+    
+    tick=datetime.now()
+    logger('info',f"Starting ARP Spoofing attack on {IP_ADDR_1} and {IP_ADDR_2} at {tick}")
+    arp_spoof(IP_ADDR_1, IP_ADDR_2)
+    tock=datetime.now()
+    logger('info',f"Finished ARP Spoofing attack on {IP_ADDR_1} and {IP_ADDR_2} at {tock}")
+    logger('info',f"Duration of ARP Spoofing attack on {IP_ADDR_1} and {IP_ADDR_2}: {tock-tick}")
+    
+    ## ARP Eveadropping attack
+    tick=datetime.now()
+    logger('info',f"Starting ARP Eveadropping attack on {IP_ADDR_1} and {IP_ADDR_2} at {tick}")
+    arp_eveadropping(IP_ADDR_1, IP_ADDR_2)
+    tock=datetime.now()
+    logger('info',f"Finished ARP Eveadropping attack on {IP_ADDR_1} and {IP_ADDR_2} at {tock}")
+    logger('info',f"Duration of ARP Eveadropping attack on {IP_ADDR_1} and {IP_ADDR_2}: {tock-tick}")
+    
+    ## ARP Blackout attack
+    tick=datetime.now()
+    logger('info',f"Starting ARP Blackout attack on {IP_ADDR_1} at {tick}")
+    arp_blackout(IP_ADDR_1)
+    tock=datetime.now()
+    logger('info',f"Finished ARP Blackout attack on {IP_ADDR_1} at {tock}")
+    logger('info',f"Duration of ARP Blackout attack on {IP_ADDR_1}: {tock-tick}")
+    
+    # Injection ATTACKS
+    ## TCP Flag Injection attack
+    tick=datetime.now()
+    logger('info',f"Starting TCP Flag Injection attack on {IP_ADDR_1} at {tick}")
+    tcp_flag_injection(IP_ADDR_1)
+    tock=datetime.now()
+    logger('info',f"Finished TCP Flag Injection attack on {IP_ADDR_1} at {tock}")
+    logger('info',f"Duration of TCP Flag Injection attack on {IP_ADDR_1}: {tock-tick}")
+    
+    ## Replay attack
+    tick=datetime.now()
+    logger('info',f"Starting Replay attack on {IP_ADDR_1} and {IP_ADDR_2} at {tick}")
+    replay_attack(IP_ADDR_1, IP_ADDR_2)
+    tock=datetime.now()
+    logger('info',f"Finished Replay attack on {IP_ADDR_1} and {IP_ADDR_2} at {tock}")
+    logger('info',f"Duration of Replay attack on {IP_ADDR_1} and {IP_ADDR_2}: {tock-tick}")
+    
+    ## FDIA attack
+    tick=datetime.now()
+    logger('info',f"Starting FDIA attack on {IP_ADDR_1} and {IP_ADDR_2} at {tick}")
+    fdia_attack(IP_ADDR_1,IP_ADDR_2)
+    tock=datetime.now()
+    logger('info',f"Finished FDIA attack on {IP_ADDR_1} and {IP_ADDR_2} at {tock}")
+    logger('info',f"Duration of FDIA attack on {IP_ADDR_1} and {IP_ADDR_2}: {tock-tick}")
+    
+    
+    logger('info',f"Finished all attacks on {IP_ADDR_1} and {IP_ADDR_2} at {tock}")
+    
+    
+    
+    
+    
